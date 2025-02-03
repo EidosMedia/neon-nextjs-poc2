c4c8c8a258c098aaac86487b5a3d5b57762431ad
samuele.ferrari <samuele.ferrari@eidosmedia.com>
2025-02-03 11:37:08 +0100

    NEON-1177 - Middleware for preview

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { getAPIHostnameConfig } from './services/utils';
+import { ResponseCookie } from 'next/dist/compiled/@edge-runtime/cookies';
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
@@ -11,6 +12,47 @@ export async function middleware(request: NextRequest) {
 
   headers.set('x-neon-backend-url', apiHostname);
   headers.set('x-neon-pathname', request.nextUrl.pathname);
+
+  const urlObject = request.nextUrl;
+  const urlParams = new URLSearchParams(urlObject.search);
+
+  if (urlParams.get('PreviewToken')) {
+    const previewToken = urlParams.get('PreviewToken');
+    const contentId = urlParams.get('id');
+    const siteName = urlParams.get('siteName');
+    const viewStatus = 'PREVIEW';
+
+    if (previewToken && contentId && siteName) {
+        const authorizationResponse = await connection.previewAuthorization(contentId, siteName, viewStatus, previewToken);
+
+        if (authorizationResponse.status !== 204) {
+            return NextResponse.json({ error: 'Internal Server Error' }, { status: authorizationResponse.status });
+        }
+
+        const cookie = authorizationResponse.headers.getSetCookie()[0];
+
+        const cookieObject = parseCookie(cookie);
+
+        const domainValue = (headers.get('x-forwarded-host') || '/').replace(':3000', '');
+        
+        const cookieValue = cookieObject.empreviewtoken;
+        const cookieOptions: ResponseCookie = {
+            path: '/',
+            maxAge: 1200,
+            httpOnly: true,
+            name: 'emauth',
+            value: cookieValue,
+            sameSite: process.env.DEV_MODE === 'false' ? 'none' : false,
+            secure: process.env.DEV_MODE === 'false',
+            domain: domainValue
+        };
+
+        const response =  NextResponse.next({ headers });
+        response.cookies.set('emauth', '', cookieOptions);
+
+        return response;
+      }
+  }
 
   if (request.nextUrl.pathname.startsWith('/resources')) {
     // need to reconstruct the full path after the domain
@@ -24,4 +66,16 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image).*)'],
+};
+
+const parseCookie = (cookieString: string): Record<string, string> => {
+  const cookieArray = cookieString.split(';');
+  const cookieObject: Record<string, string> = {};
+
+  cookieArray.forEach(cookie => {
+      const cookiePair = cookie.split('=');
+      cookieObject[cookiePair[0]] = cookiePair[1];
+  });
+
+  return cookieObject;
 };
