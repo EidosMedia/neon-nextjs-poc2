7dc5acc046d850231e86fed769f77eeda5d6cbb6
samuele.ferrari <samuele.ferrari@eidosmedia.com>
2025-02-06 14:52:02 +0100

    NEON-1184 - Using components

--- src/app/_pages/webpage.tsx
+++ src/app/_pages/webpage.tsx
@@ -1,64 +1,19 @@
 import { PageData } from "@eidosmedia/neon-frontoffice-ts-sdk";
 import { WebpageModel } from "@eidosmedia/neon-frontoffice-ts-sdk";
 import Navbar from "../components/Navbar";
-import { NeonConnection } from "@eidosmedia/neon-frontoffice-ts-sdk";
 import TopSection from "../components/TopSection";
+import BannerSection from "../components/BannerSection";
 
 type PageProps = {
   data: PageData<WebpageModel>;
 };
 
-declare global {
-  // eslint-disable-next-line no-var
-  var connection: NeonConnection;
-}
-
 const Webpage: React.FC<PageProps> = async ({ data }) => {
-  const linkedObjects = await connection.getDwxLinkedObjects(data, "banner");
-  const linkedObjectstop = await connection.getDwxLinkedObjects(data, "top");
-
   return (
     <div className="container mx-auto">
       <Navbar data={data}></Navbar>
-      {linkedObjects.map((linkedObject: any, index: number) => (
-        <div key={linkedObject.id} className="p-4">
-          <a className="no-underline" href={linkedObjects[`${index}`].url}>
-            <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-              <div className="flex items-center space-x-2">
-                <svg
-                  className="w-6 h-6 text-gray-500"
-                  focusable="false"
-                  aria-hidden="true"
-                  viewBox="0 0 24 24"
-                  data-testid="CircleIcon"
-                >
-                  <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2z"></path>
-                </svg>
-                <p className="text-base font-medium text-gray-700">
-                  BREAKING NEWS
-                </p>
-              </div>
-              <h6 className="mt-2 text-lg font-semibold text-gray-900">
-                {linkedObject.title}
-              </h6>
-            </div>
-          </a>
-        </div>
-      ))}
-
-      {linkedObjectstop.map((linkedObjecttop: any, index: number) => (
-          <TopSection
-              key={linkedObjecttop.id}
-              linkedObjecttop={{
-                id: linkedObjecttop.id,
-                title: linkedObjecttop.title,
-                imageUrl: linkedObjecttop.links.system.mainPicture[0].dynamicCropsResourceUrls.Portrait_small,
-                link: linkedObjecttop[`${index}`].url,
-                summary: linkedObjecttop.summary
-              }}
-          />
-      ))}
-
+      <BannerSection data={data} />
+      <TopSection data={data} />
     </div>
   );
 };
--- src/app/components/TopSection.tsx
+++ src/app/components/TopSection.tsx
@@ -1,37 +1,42 @@
 import React from "react";
+import { PageData, WebpageModel } from "@eidosmedia/neon-frontoffice-ts-sdk";
 
-type CardProps = {
-  linkedObjecttop: {
-    id: string;
-    title: string;
-    imageUrl: string;
-    link: string;
-    summary: string;
-  };
+type TopSectionProps = {
+  data: PageData<WebpageModel>;
 };
 
-const TopSection: React.FC<CardProps> = ({ linkedObjecttop }) => {
+const TopSection: React.FC<TopSectionProps> = async ({ data }) => {
+  const linkedObjects = await connection.getDwxLinkedObjects(data, "top");
+
   return (
-      <div key={linkedObjecttop.id} className="p-4">
-        <div className="p-4 bg-gray-100 rounded-lg shadow-md">
-          <a href={linkedObjecttop.link}>
-            <img
+    <>
+      {linkedObjects.map((linkedObject: any, index: number) => (
+        <div key={linkedObject.id} className="p-4">
+          <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+            <a href={linkedObjects[`${index}`].url}>
+              <img
                 alt="/static/img/nothumb.jpeg"
                 width={550}
                 height={287}
                 decoding="async"
-                className="w-full h-auto"
-                src={linkedObjecttop.imageUrl}
-            />
-            <div className="p-4">
-              <h6 className="text-lg font-semibold text-gray-900">
-                {linkedObjecttop.title}
-              </h6>
-              <p className="text-base text-gray-700">{linkedObjecttop.summary}</p>
-            </div>
-          </a>
+                src={
+                  linkedObject.links.system.mainPicture[0]
+                    .dynamicCropsResourceUrls.Portrait_small
+                }
+              />
+              <div className="p-4">
+                <h6 className="text-lg font-semibold text-gray-900">
+                  {linkedObject.title}
+                </h6>
+                <p className="text-base text-gray-700">
+                  {linkedObject.summary}
+                </p>
+              </div>
+            </a>
+          </div>
         </div>
-      </div>
+      ))}
+    </>
   );
 };
 
