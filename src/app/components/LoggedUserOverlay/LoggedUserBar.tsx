452338fb8b656ce416e32521f0f48ca99e5a957e
Valerio Cassani <valerio.cassani@eidosmedia.com>
2025-03-06 15:28:49 +0100

    NEON: extracted swing component

--- src/app/components/LoggedUserOverlay/LoggedUserBar.tsx
+++ src/app/components/LoggedUserOverlay/LoggedUserBar.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { NeonConnection, SiteNode } from '@eidosmedia/neon-frontoffice-ts-sdk';
 import React, { useEffect, useState } from 'react';
+import Switch from '../base/Switch';
 
 interface User {
   name: string;
@@ -39,42 +40,26 @@ const LoggedUserBar: React.FC<LoggedUserBarProps> = ({ data }) => {
     setSwitch2Enabled(!switch2Enabled);
   };
 
-  if (!currentUser) {
-    return null;
-  }
+  // if (!currentUser) {
+  //   return null;
+  // }
 
   return (
-    <div className="relative flex items-center bg-black h-16 justify-between ">
+    <div className="relative flex items-center bg-(--color-toolbar-background) h-16 justify-between ">
       <div className="flex items-center">
         <div className="flex items-center justify-center bg-gray-500 text-white p-5">
           PREVIEW
         </div>
-
-        <div className="flex items-center mx-4">
-          <label className="relative inline-flex items-center cursor-pointer">
-            <input
-              type="checkbox"
-              checked={switch1Enabled}
-              onChange={toggleSwitch1}
-              className="sr-only peer"
-            />
-            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
-          </label>
-          <span className="m-3 text-white">Inspect items</span>
-        </div>
-
-        <div className="flex items-center mx-4">
-          <label className="relative inline-flex items-center cursor-pointer">
-            <input
-              type="checkbox"
-              checked={switch2Enabled}
-              onChange={toggleSwitch2}
-              className="sr-only peer"
-            />
-            <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
-          </label>
-          <span className="m-3 text-white">Analytics</span>
-        </div>
+        <Switch
+          label="Inspect items"
+          checked={switch1Enabled}
+          onChange={toggleSwitch1}
+        />
+        <Switch
+          label="Analytics"
+          checked={switch2Enabled}
+          onChange={toggleSwitch2}
+        />
       </div>
       <div className="flex items-center">
         <div className="flex items-center justify-center text-white">
