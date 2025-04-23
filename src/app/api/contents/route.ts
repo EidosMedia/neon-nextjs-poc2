655f1ae7432fa11c83c25929c68d87300778b42c
caio.cristo <caio.cristo@eidosmedia.com>
2025-04-23 12:22:46 +0100

    NEON-1277: Frontoffice changes

--- src/app/api/contents/route.ts
+++ src/app/api/contents/route.ts
@@ -29,14 +29,14 @@ export async function DELETE(request: NextRequest) {
     const cookiesFromRequest = await cookies();
     const previewtoken = cookiesFromRequest.get('previewtoken')?.value;
 
-    const promoteContentLive = await connection.unpromoteContentLive({
+    const unpromoteContentLive = await connection.unpromoteContentLive({
       id: id,
       headers: {
         Authorization: `Bearer ${previewtoken}`,
       },
       baseUrl: process.env.BASE_NEON_FE_URL || '',
     });
-    return Response.json({ ...promoteContentLive });
+    return Response.json({ ...unpromoteContentLive });
   } catch (error) {
     console.log(error);
     return Response.json({ error: 'Internal Server Error' }, { status: 500 });
