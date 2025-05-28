fca66933167bf16ba04298bb73d9c409a3ba0bb2
Davide Compagnone <davide.compagnone@eidosmedia.com>
2025-05-28 15:51:50 +0200

    NEON-1385 - render teaser

--- src/app/components/contentElements/MainImage.tsx
+++ src/app/components/contentElements/MainImage.tsx
@@ -1,5 +1,5 @@
-import { ArticleModel } from '@/types/models';
-import { findElementsInContentJson, renderContent } from '@/utilities/content';
+import {ArticleModel} from '@/types/models';
+import {findElementsInContentJson, renderContent} from '@/utilities/content';
 
 type MainImageProps = {
   data: ArticleModel;
@@ -7,33 +7,32 @@ type MainImageProps = {
   hideCaptions?: boolean;
 };
 
-const getMainImageUrl = (data: ArticleModel, format: string): string | undefined => {
+const getTeaserOrMainImageUrl = (data: ArticleModel, format: string): string | undefined => {
+  const teaserPicture = data?.links?.system?.teaserPicture?.[0];
   const mainPicture = data?.links?.system?.mainPicture?.[0];
-  return mainPicture?.dynamicCropsResourceUrls?.[format];
+
+  return teaserPicture?.dynamicCropsResourceUrls?.[format]
+      ?? mainPicture?.dynamicCropsResourceUrls?.[format];
 };
 
 const MainImage: React.FC<MainImageProps> = ({ data, format, hideCaptions }) => {
-  let imageWidth = 1024;
-  let imageHeight = 576;
+  const mainImageUrl = getTeaserOrMainImageUrl(data, format || 'Wide_large');
 
-  const mainImageUrl = getMainImageUrl(data, format || 'Wide_large');
-
-  const render = (
-    <div>
+  return (
       <div>
-        {mainImageUrl ? (
-          <div>
-            <img src={mainImageUrl} alt="" />
-            {!hideCaptions &&
-              renderContent(findElementsInContentJson(['web-image-caption'], data.files.content.data)[0])}
-          </div>
-        ) : (
-          <p>No main image available</p>
-        )}
+        <div>
+          {mainImageUrl ? (
+              <div>
+                <img src={mainImageUrl} alt=""/>
+                {!hideCaptions &&
+                    renderContent(findElementsInContentJson(['web-image-caption'], data.files.content.data)[0])}
+              </div>
+          ) : (
+              <p>No main image available</p>
+          )}
+        </div>
       </div>
-    </div>
   );
-  return render;
 };
 
 export default MainImage;
