def8a2a99219459067068d79ac9fd372874cbcd1
Abderraouf Rahmani <abderraouf.rahmani@RAHMANI-MAC.local>
2025-05-30 16:02:46 +0100

    Implimented a customizable Select component

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -12,7 +12,7 @@ export async function register() {
   //   // await require('next-logger');
   // }
 
-  if (!process.env.BASE_NEON_FO_URL) {
+  if (!process.env.BASE_NEON_FE_URL) {
     throw new Error('BASE_NEON_FO_URL not specified in any .env file');
   }
 
@@ -22,7 +22,7 @@ export async function register() {
 
   const connection = new NeonConnection({
     
-    neonFoUrl: process.env.BASE_NEON_FO_URL,
+    neonFoUrl: process.env.BASE_NEON_FE_URL,
     frontOfficeServiceKey: process.env.NEON_FRONTOFFICE_SERVICE_KEY || '',
   });
 
