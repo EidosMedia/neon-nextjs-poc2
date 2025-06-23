5d3dd7089f79cde9ce9bb10ed1ba5aa2f406a4a1
samuele.ferrari <samuele.ferrari@eidosmedia.com>
2025-06-23 15:46:27 +0200

    NEON-1447 - Removed unused file

--- src/services/services.ts
+++
@@ -1,5 +0,0 @@
-export const getPageData = async (hostname: string, slug: string[]) => {
-  const pageData = await fetch(`${hostname}/${slug.join('/')}`);
-  const pageDataJSON = await pageData.json();
-  return pageDataJSON;
-};
