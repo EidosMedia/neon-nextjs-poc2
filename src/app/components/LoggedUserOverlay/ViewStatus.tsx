25830d0b7bd90884cdd73ca197731143b6cc3907
Davide Compagnone <davide.compagnone@eidosmedia.com>
2025-08-21 12:57:15 +0200

    NEON-1487 - fix button to manage switch (live/preview) from poc

--- src/app/components/LoggedUserOverlay/ViewStatus.tsx
+++ src/app/components/LoggedUserOverlay/ViewStatus.tsx
@@ -63,20 +63,17 @@ const ViewStatus: React.FC<LoggedUserBarProps> = ({ data }) => {
           </div>
         )}
       </div>
-
       {(isLastLive || isLastPreview) && 'model' in data && data.model.data.url && (
-        <div className={clsx('flex items-center justify-center text-white px-5 h-16', colorClass())}>
-          <Link
-            href="#"
-            className="no-underline"
-            onClick={e => {
-              e.preventDefault();
-              window.location.href = goToUrl();
-            }}
-          >
-            {isLastLive ? 'GO TO PREVIEW' : 'GO TO LIVE'}
-          </Link>
-        </div>
+        <Link
+          href="#"
+          className="fit-content cursor-pointer px-4 py-2 rounded-[2px] text-white bg-[#2847E2] hover:bg-[#191FBD] duration-300 ease-in-out ml-4"
+          onClick={e => {
+            e.preventDefault();
+            window.location.href = goToUrl();
+          }}
+        >
+          {isLastLive ? 'GO TO PREVIEW' : 'GO TO LIVE'}
+        </Link>
       )}
     </>
   );
